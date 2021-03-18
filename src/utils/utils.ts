c5000db5c02593eb78a8f77cf242ed2b6f117760
ilkou <ouklichoussama@gmail.com>
2021-03-18 23:22:25 +0100

    utils :rocket:

---
+++ src/utils/utils.ts
@@ -0,0 +1,20 @@
+export const isOperator = (c: string): boolean => {
+    return /[+\-*\/\^%=(),]/.test(c);
+};
+
+export const isDigit = (c: string): boolean => {
+    return /[0-9]/.test(c);
+};
+
+export const isWhiteSpace = (c: string): boolean => {
+    return /\s/.test(c);
+};
+
+export const isIdentifier = (c: string): boolean => {
+    return (
+        typeof c === "string" &&
+        !isOperator(c) &&
+        !isDigit(c) &&
+        !isWhiteSpace(c)
+    );
+};
